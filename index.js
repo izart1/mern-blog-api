8f095f11612597ef9ed6f505f480daf91b4a8d67
Artem Izmailov <77829898+izart1@users.noreply.github.com>
2023-01-21 02:55:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-      if (!fs.existsSync('uploads')) {
-      fs.mkdir('uploads');
+     if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
     }
     cb(null, 'uploads');
   },
